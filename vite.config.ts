21f442dc70407d6b96518975fd2898f7d00fd005
Rakhim <79516471+Rakhim-Mutaliev@users.noreply.github.com>
2026-08-20 19:10:42 +0300

    Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/AR-Room-Demo/',
   plugins: [react()],
   server: { host: true },
   preview: { host: true },
